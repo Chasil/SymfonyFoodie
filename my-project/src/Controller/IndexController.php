01a2f9305f1d7dffb8e68e96a56aa3add7fd080e
mwojcikbs <mwojcikbs@gmail.com>
2023-01-04 10:47:33 +0100

    save recipie from the form

--- my-project/src/Controller/IndexController.php
+++ my-project/src/Controller/IndexController.php
@@ -19,68 +19,71 @@ class IndexController extends AbstractController
     public function index(Request $request, ManagerRegistry $doctrine): Response
     {
 
-        $apiURL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
-        $jsonData = file_get_contents($apiURL);
-        $responseData = json_decode($jsonData);
-
         $form = $this->createForm(AddRecipieType::class);
-
         $form->handleRequest($request);
-        if($responseData) {
 
-            $doctrineManager = $doctrine->getManager();
+        if($form->isSubmitted()) {
 
-            if($this->getUser()) {
+            $apiURL = $form->get('meal_link')->getData();
 
-                foreach($responseData as $meals) {
-                    foreach($meals as $meal) {
+            if($apiURL) {
 
-                        $entityRecipy = new Recipie();
+                $jsonData = file_get_contents($apiURL);
+                $responseData = json_decode($jsonData);
 
-                        $entityRecipy->setName($meal->strMeal);
-                        $entityRecipy->setDescription("");
-                        $entityRecipy->setPreparation($meal->strInstructions);
-                        $entityRecipy->setCategory($meal->strCategory);
-                        $entityRecipy->setPhoto($meal->strMealThumb);
-                        $entityRecipy->setIsVisible(1);
-                        $entityRecipy->setUser($this->getUser());
+                if($responseData) {
 
-                        $tags = explode(",", $meal->strTags);
+                    $doctrineManager = $doctrine->getManager();
 
-                        foreach($tags as $tag) {
-                            $entityTags = new Tags();
-                            $tagObject = $entityTags->setName($tag);
-                            $tagObject->setRecipie($entityRecipy);
-                            $doctrineManager->persist($entityTags);
-                        }
+                    if ($this->getUser()) {
 
-                        $ingredients = [];
-                        $maxIngredients = 20;
+                        foreach ($responseData as $meals) {
+                            foreach ($meals as $meal) {
 
-                        for($iterateIngredients = 1; $iterateIngredients <= $maxIngredients; $iterateIngredients++) {
-                            $ingredient = $meal->{'strIngredient' . $iterateIngredients};
-                            if(($ingredient || !empty($ingredient)) && $iterateIngredients <= $maxIngredients) {
-                                $ingredients[] = $ingredient;
+                                $entityRecipy = new Recipie();
+
+                                $entityRecipy->setName($meal->strMeal);
+                                $entityRecipy->setDescription("");
+                                $entityRecipy->setPreparation($meal->strInstructions);
+                                $entityRecipy->setCategory($meal->strCategory);
+                                $entityRecipy->setPhoto($meal->strMealThumb);
+                                $entityRecipy->setIsVisible(1);
+                                $entityRecipy->setUser($this->getUser());
+
+                                $tags = explode(",", $meal->strTags);
+
+                                foreach ($tags as $tag) {
+                                    $entityTags = new Tags();
+                                    $tagObject = $entityTags->setName($tag);
+                                    $tagObject->setRecipie($entityRecipy);
+                                    $doctrineManager->persist($entityTags);
+                                }
+
+                                $ingredients = [];
+                                $maxIngredients = 20;
+
+                                for ($iterateIngredients = 1; $iterateIngredients <= $maxIngredients; $iterateIngredients++) {
+                                    $ingredient = $meal->{'strIngredient' . $iterateIngredients};
+                                    if (($ingredient || !empty($ingredient)) && $iterateIngredients <= $maxIngredients) {
+                                        $ingredients[] = $ingredient;
+                                    }
+                                    $iterateIngredients++;
+                                }
+
+                                foreach ($ingredients as $ingredient) {
+                                    $entityIngredients = new Ingredients();
+                                    $ingredientObject = $entityIngredients->setName($ingredient);
+                                    $ingredientObject->setRecipie($entityRecipy);
+                                    $doctrineManager->persist($entityIngredients);
+                                }
+
+                                $doctrineManager->persist($entityRecipy);
+                                $doctrineManager->flush();
                             }
-                            $iterateIngredients++;
                         }
-
-                        foreach($ingredients as $ingredient) {
-                            $entityIngredients = new Ingredients();
-                            $ingredientObject = $entityIngredients->setName($ingredient);
-                            $ingredientObject->setRecipie($entityRecipy);
-                            $doctrineManager->persist($entityIngredients);
-                        }
-
-                        $doctrineManager->persist($entityRecipy);
-                        $doctrineManager->flush();
-
+                        $this->addFlash('notice', 'Saved succeeded');
                     }
                 }
-
-
-                $this->addFlash('notice', 'Recipy added successfully');
-
             }
         }
 
