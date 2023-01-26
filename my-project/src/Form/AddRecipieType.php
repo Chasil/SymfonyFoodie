7699de05e29a06b9c62304c4a29ef2114d8c3f72
mwojcikbs <mwojcikbs@gmail.com>
2023-01-26 15:13:35 +0100

    fix form

--- my-project/src/Form/AddRecipieType.php
+++ my-project/src/Form/AddRecipieType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Controller\Admin\PanelController;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +15,7 @@ class AddRecipieType extends AbstractType
     {
         $builder
             ->add('meal_link', TextType::class, ['mapped' => false])
+            ->add('save', SubmitType::class)
         ;
     }
 
