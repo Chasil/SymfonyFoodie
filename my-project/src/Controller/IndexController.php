f1ee3ea0dc159a79373c90145f5127cc6242b710
mwojcikbs <mwojcikbs@gmail.com>
2023-02-13 10:42:35 +0100

    add pagination

--- my-project/src/Controller/IndexController.php
+++ my-project/src/Controller/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Entity\Recipie;
 use App\Entity\Tag;
+use App\Repository\RecipieRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,18 +13,37 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(ManagerRegistry $doctrine): Response
+    #[Route('/{page}', name: 'index', requirements: ['page' => '\d+'])]
+    public function index(ManagerRegistry $doctrine, $page = 1): Response
     {
+        $perPage = 5;
+
         $doctrineManager = $doctrine->getManager();
-        $recipies = $doctrineManager->getRepository(Recipie::class)->findBy(['isVisible' => 1]);
+        /** @var RecipieRepository $recipieRepository */
+        $recipieRepository = $doctrineManager->getRepository(Recipie::class);
+        $findCriteria = ['isVisible' => 1];
+        $pageCount = $recipieRepository->count($findCriteria) / $perPage;
+
+        if ($page - 1 > $pageCount) {
+            throw new \Exception();
+        }
+
+        $recipies = $recipieRepository->findBy(
+            $findCriteria,
+            null,
+            $perPage,
+            $perPage * ($page-1)
+        );
+
         $categories = $doctrineManager->getRepository(Category::class)->findGroupedCategories();
         $tags = $doctrineManager->getRepository(Tag::class)->findAll();
 
         return $this->render('index/index.html.twig', [
             'recipies' => $recipies,
             'categories' => $categories,
-            'tags' => $tags
+            'tags' => $tags,
+            'page' => $page,
+            'totalPages' => $pageCount
         ]);
     }
 }
